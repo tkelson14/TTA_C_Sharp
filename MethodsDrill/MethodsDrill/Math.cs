4cc63dc1193e9f9e464518c8409f6b4e6b597690
Todd Kelson <toddjkelson@gmail.com>
2019-08-21 11:31:09 -0600

    Method Drill

---
+++ MethodsDrill/MethodsDrill/Math.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill
+{
+    public class Math
+    {
+        public Math()
+        {
+            Value = 7;
+        }
+        public int Value { get; set; }
+
+        public static int addTwoInts(int n) 
+        {
+            Console.WriteLine("Adding Numbers...");
+            int result = n + 4;
+            Console.WriteLine(result);
+            return result;
+        }
+
+        public static int subTwoInts(int n)
+        {
+            Console.WriteLine("Subtracting Numbers...");
+            int result = n - 2;
+            Console.WriteLine(result);
+            return result;
+        }
+        public static int multTwoInts(int n)
+        {
+            Console.WriteLine("Multiplying Numbers...");
+            int result = n * 6;
+            Console.WriteLine(result);
+            return result;
+        }
+    }
+}
---
+++ MethodsDrill/MethodsDrill/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Math userNum = new Math();
+            Console.WriteLine("Chose an integer.");      
+            userNum.Value = Convert.ToInt32(Console.ReadLine());
+            Math.addTwoInts(userNum.Value); 
+            Math.subTwoInts(userNum.Value);
+            Math.multTwoInts(userNum.Value);
+            Console.ReadLine();
+
+        }
+    }
+}
