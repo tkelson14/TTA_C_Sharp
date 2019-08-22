6de9c2e290a8436fd85f9a1d17586b5135c07e3b
Todd Kelson <toddjkelson@gmail.com>
2019-08-22 09:23:11 -0600

    Methods Drill 2

---
+++ MethodsDrill2/MethodsDrill2/Numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill2
+{
+    class Void_Method
+    {
+        public void Pass1(int number1, int number2)
+        {
+            int total = number1 + 12; //math operation on first integer, user doesn't see it
+            Console.WriteLine("Here is the second integer : " + number2); //outputs only 2nd integer to console
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodsDrill2/MethodsDrill2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Void_Method obtainNumber = new Void_Method(); //Instantiate a class
+            obtainNumber.Pass1(10, 20); //Passing in two integers
+        }
+    }
+}
