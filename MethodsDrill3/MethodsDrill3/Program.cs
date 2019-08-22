217a19a4520a07480c4580d91cc7e83b133476b6
Todd Kelson <toddjkelson@gmail.com>
2019-08-22 10:07:18 -0600

    Methods Drill 3

---
+++ MethodsDrill3/MethodsDrill3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathFunctions mathOperations = new mathFunctions();
+            int num1 = 12;
+
+            int resultOne = mathOperations.addition(num1);
+
+            decimal resultTwo = mathOperations.addition(15m);
+
+            int resultThree = mathOperations.addition("15");
+
+            Console.WriteLine(resultOne);
+            Console.WriteLine(resultTwo);
+            Console.WriteLine(resultThree);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MethodsDrill3/MethodsDrill3/mathFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsDrill3
+{
+    class mathFunctions
+    {
+        public int addition(int num1)
+        {
+            int results = num1 + 5;
+            return results;
+        }
+        public decimal addition(decimal num1)
+        {
+            decimal results = num1 * 9;
+            return Convert.ToInt32(results);
+        }
+        public int addition(string num1)
+        {
+            int results = Convert.ToInt32(num1) + 15;
+            return results;
+        }
+    }
+}
