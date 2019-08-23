f514cce6bb02111b0e1c10ff8c3c065d727c48c7
Todd Kelson <toddjkelson@gmail.com>
2019-08-23 13:35:41 -0600

    Abstract Drill

---
+++ AbstractDrill/AbstractDrill/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDrill
+{
+    public class Employee : Person
+    {
+        public override void SayName()
+        {
+            Console.WriteLine("The employee's name is : {0} {1}", firstName, lastName);
+        }
+    }
+}
---
+++ AbstractDrill/AbstractDrill/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractDrill
+{
+    public abstract class Person
+    {
+        public string firstName;
+        public string lastName;
+        public abstract void SayName();
+    }
+}
