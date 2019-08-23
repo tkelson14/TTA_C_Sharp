1d2a7dc17513c6a9a13b6e594d8204a0a6ab3d58
Todd Kelson <toddjkelson@gmail.com>
2019-08-23 11:37:33 -0600

    Class Method Drill

---
+++ ClassMethod/ClassMethod/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathFunction userNum = new mathFunction();
+            Console.WriteLine("Please enter an interger.");
+
+            try
+            {
+                int userInput = Convert.ToInt32(Console.ReadLine()); 
+                mathFunction.divide(userInput); 
+                mathFunction.divide(userInput, out userInput); 
+                Console.ReadLine();
+            }
+            
+            catch (FormatException)
+            {
+                Console.WriteLine("Enter only an integer.");
+            }
+            
+        }
+    }
+}
---
+++ ClassMethod/ClassMethod/mathFunction.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethod
+{
+    class mathFunction
+    {
+       
+        public static void divide(int inNum)
+        {
+            int total = inNum / 2;
+            Console.WriteLine(total);
+        }
+        public static void divide(int inNum1, out int outNum2)
+        {
+            int total = outNum2 = inNum1 / 2;
+            //Console.WriteLine(total);
+        }
+        public static class empty
+        {
+
+        }
+    }
+}
