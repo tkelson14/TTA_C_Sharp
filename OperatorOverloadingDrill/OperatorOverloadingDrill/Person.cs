d5207e08e1538a7bcfa136cfd710fd0a22e9caf0
Todd Kelson <toddjkelson@gmail.com>
2019-08-26 13:43:49 -0600

    Operator Overloading Drill

---
+++ OperatorOverloadingDrill/OperatorOverloadingDrill/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingDrill
+{
+    public class Person : Employee
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int ID { get; set; }
+
+        public void SayName()
+        {
+            Console.WriteLine("Employee Name: {0} {1} Employee ID: {2}" , FirstName, LastName, ID);
+        }
+        public static bool operator ==(Person person1, Person person2)
+        {
+            bool status = false;
+            if (person1.ID == person2.ID)
+            {
+                status = true;
+            }
+            return status;
+        }
+        public static bool operator !=(Person person1, Person person2)
+        {
+            bool status = false;
+            if (person1.ID != person2.ID)
+            {
+                status = true;
+            }
+            return status;
+        }
+
+    }
+}
---
+++ OperatorOverloadingDrill/OperatorOverloadingDrill/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorOverloadingDrill
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Wade";
+            person1.LastName = "Nelson";
+            person1.ID = 22;
+            person1.SayName();
+            Console.ReadLine();
+
+            Person person2 = new Person();
+            person2.FirstName = "Peter";
+            person2.LastName = "Rasput";
+            person2.ID = 11;
+            person2.SayName();
+            Console.ReadLine();
+
+            if (person1 == person2)
+                Console.WriteLine("Wade Nelson and Peter Rasput have the same Employee IDs.");
+            else
+                Console.WriteLine("Wade Nelson and Peter Rasput do not have the same Employee IDs.");
+            Console.ReadLine();
+        }
+    }
+}
