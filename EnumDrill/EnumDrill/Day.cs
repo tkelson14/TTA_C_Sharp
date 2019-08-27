9f476bb5706c97fba28e06f7f98d944dcd8495e5
Todd Kelson <toddjkelson@gmail.com>
2019-08-27 11:05:03 -0600

    Enum Drill

---
+++ EnumDrill/EnumDrill/Day.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDrill
+{
+    enum Days { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday };
+    public class Day
+    {
+        public enum Days
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+    }
+    
+}
---
+++ EnumDrill/EnumDrill/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter current day of the week: ");
+                string day = Convert.ToString(Console.ReadLine());
+                Day.Days eDay = (Day.Days)Enum.Parse(typeof(Day.Days), day);
+
+                Console.WriteLine(eDay + " is a day of the week");
+                Console.ReadLine();
+
+            }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
