db7566a9d5b36545a2b2f53b8b26df9e0c71456a
Todd Kelson <toddjkelson@gmail.com>
2019-08-28 13:00:25 -0600

    File IO Drill

---
+++ FileIODrill/FileIODrill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIODrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide a number.");
+            string num = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\Mini\Downloads\log.txt", num);
+            Console.WriteLine("Here is your number {0}", num);
+            Console.ReadLine();
+        }
+    }
+}
