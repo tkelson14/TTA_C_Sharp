553e916b56433c4f6e4d5dc468312e0fb79647da
Todd Kelson <toddjkelson@gmail.com>
2019-08-29 09:57:47 -0600

    DateTime Drill

---
+++ DateTimeDrill/DateTimeDrill/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeDrill
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+            Console.WriteLine("Please enter a number.");
+            int num = Convert.ToInt32(Console.ReadLine());
+            DateTime myDateTime = DateTime.Now.AddHours(num);            
+            Console.WriteLine("It will be {0} in {1} hours.", myDateTime, num);
+            Console.ReadLine();
+        }
+    }
+}
