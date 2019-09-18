eb058432d61018d3b2f051093f3cf920c34252dd
Todd Kelson <toddjkelson@gmail.com>
2019-09-18 10:12:49 -0600

    Car Insurance

---
+++ CarInsurance/CarInsurance/Controllers/AdminController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CarInsurance;
+
+namespace CarInsurance.Controllers
+{
+    public class AdminController : Controller
+    {
+        private CarInsuranceEntities db = new CarInsuranceEntities();
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View(db.Quotes.ToList());
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Quote Quote = db.Quotes.Find(id);
+            if (Quote == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Quote);
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,FirstName,LastName,EmailAddress,CarYear,CarMake,CarModel,DUI,FullCoverageLiability,Quotes,DateOfBirth")] Quote Quote)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Quotes.Add(Quote);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(Quote);
+        }
+
+        // GET: Admin/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Quote Quote = db.Quotes.Find(id);
+            if (Quote == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Quote);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,FirstName,LastName,EmailAddress,CarYear,CarMake,CarModel,DUI,FullCoverageLiability,Quotes,DateOfBirth")] Quote Quote)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(Quote).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Quote);
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Quote Quote = db.Quotes.Find(id);
+            if (Quote == null)
+            {
+                return HttpNotFound();
+            }
+            return View(Quote);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Quote Quote = db.Quotes.Find(id);
+            db.Quotes.Remove(Quote);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CarInsurance/CarInsurance/Controllers/HomeController.cs
@@ -0,0 +1,174 @@
+﻿using CarInsurance.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarInsurance.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() //display information to user, total price
+        {
+            return View();
+        }
+
+        [HttpPost]//Parameter name must be the same as the inputname in index.cshtml otherwise error
+        public ActionResult SignUp(string firstName, string lastName, string emailAddress, DateTime dateOfBirth, int carYear, string carMake, string carModel, string dUI, int speedingTickets, int fullCoverageLiability)
+        {
+            decimal quoteTotal = 50;// start with a base of $50 
+            using (CarInsuranceEntities db = new CarInsuranceEntities())// instantiate the partial class, allow objects to access the database
+            {
+                var signup = new Quote();//new object from partial class === why?
+
+
+                int userAge = Convert.ToDateTime(dateOfBirth).Year;
+                int timeNow = Convert.ToDateTime(DateTime.Today).Year;
+                int ageDiff = timeNow - userAge;
+
+                //============== code intentions ============
+                //> obtain exact age then compare it with system date using month day and year
+                //e.g. DOB = 5/5/1995. Today = 5/4/2019. age = 24 but they are 23 years old not 24.
+
+                //DateTime now = DateTime.Today
+                //int age = now.Year - dateOfBirth.Year;
+
+                //if (dateOfBirth < now.AddYears(-age)) age--;// subtract age 
+                //{
+
+                //    quoteTotal += 100;
+                //}
+                //else if (dateOfBirth > 18 && dateOfBirth < 25)
+                //{
+                //    quoteTotal += 25;
+                //}
+
+                //user age 
+                //> will calculate only the year not month or day 
+                if (ageDiff < 18)
+                {
+                    quoteTotal += 100;
+                }
+                else if (ageDiff > 18 && ageDiff < 25)
+                {
+                    quoteTotal += 25;
+                }
+                else if (ageDiff > 100)
+                {
+                    quoteTotal += 25;
+                }
+
+                //car year 
+                if (carYear < 2000 || carYear > 2015)//add 25 
+                {
+                    quoteTotal += 25;
+                }
+
+                //car make
+                if (carMake.ToLower().Contains("porsche") && carModel.ToLower().Contains("911 carrera"))//multiple conditions must come first otherwise a single condition will only execute like the if else condition
+                {
+                    quoteTotal += 50;
+                }
+                else if (carMake.ToLower().Contains("porsche"))//checks and lowers user input 
+                {
+                    quoteTotal += 25;
+                }
+
+                //Speeding Tickets
+                if (speedingTickets == 0)
+                {
+                    speedingTickets *= 10; //for each tickets multiply by 10 and add to pending total
+                }
+                else
+                {
+                    speedingTickets *= 10;
+                    quoteTotal += speedingTickets;
+                }
+
+                //DUI add 10%
+                if (dUI.ToLower().Contains("yes"))// if yes pending total is nulyiply by 0.25
+                {
+                    decimal DuiCharge = quoteTotal * 0.25m;
+                    quoteTotal += DuiCharge;
+                }
+                else if (dUI.ToLower().Contains("no"))//does not return value 
+                {
+                    quoteTotal += 0;
+                }
+
+                //Full coverage or liability
+                if (fullCoverageLiability == 1)
+                {
+                    decimal fullCoverage = quoteTotal * 0.50m;
+                    quoteTotal += fullCoverage;
+                }
+                else if (fullCoverageLiability == 0)//does not return value
+                {
+                    quoteTotal += 0;
+                }
+
+
+                signup.FirstName = firstName;
+                signup.LastName = lastName;
+                signup.EmailAddress = emailAddress;
+                signup.DateOfBirth = dateOfBirth;
+                signup.CarYear = carYear;
+                signup.CarMake = carMake;
+                signup.CarModel = carModel;
+                signup.DUI = dUI;
+                signup.SpeedingTickets = speedingTickets;
+                signup.FullCoverageLiability = fullCoverageLiability;
+                signup.Quotes = Convert.ToInt32(quoteTotal);
+
+
+                db.Quotes.Add(signup);//pass signup object 
+                db.SaveChanges();
+            }
+
+            CredentialsVm newQuote = new CredentialsVm();
+            newQuote.FirstName = firstName;
+            newQuote.LastName = lastName;
+            newQuote.EmailAddress = emailAddress;
+            newQuote.Quotes = quoteTotal;
+            return View(newQuote);
+
+            //return View("~/Views/Home/Quote.cshtml");
+            //return RedirectToAction("Quote", "Home");//redirects to Quote to perform the logic after user enters information
+        }
+
+        //========= Intentions for code to work ========
+
+        //> use the actionresult signup to redirect to this actionresult Quote for organization purposes(separation between logical and user input)
+        //> unfortunately I was not able to figure out how to retrieve data and use that data to do logic
+        //> Intentions> grab the highest Id (because everytime a user submits info to the DB the id increments by 1) and select the column name DateOfBirth in the database using highest Id
+
+        //public ActionResult Quote(DateTime? DateOfBirth)//pass Quote, DateOfBirth inside ()
+        //{
+
+        //    using (CarInsuranceEntities db = new CarInsuranceEntities())
+        //    {
+
+        //        //Quote Quote = new Quote();
+        //        
+
+        //        //signup.DateOfBirth = DateTime.Now.ToString("dd/MM/yyyy"); //was considering using this code but not in use for this purposes
+        //        //var dateofBirth = DateTime.Now.year;
+
+        //        Quote Quote = new Quote();
+        //        int maxId = db.Quotes.Max(p => p.Id).CompareTo; 
+        //        var t = DateOfBirth;
+
+        //        int quoteTotal = 50;
+
+        //        if (DateTime.Now < DateOfBirth)
+        //        {
+        //            quoteTotal += 25;
+        //        }
+        //        db.Quotes.Add(Quote);
+
+        //    }
+        //    return View("~/Views/Home/Quote.cshtml");//redirects from home controller to Quote then to total amount 
+        //}
+    }
+}
