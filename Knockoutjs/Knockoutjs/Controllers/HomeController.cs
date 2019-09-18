d1226050ee42d40bd24835d5662b8dcb56c1db79
Todd Kelson <toddjkelson@gmail.com>
2019-09-18 10:47:45 -0600

    Knockoutjs

---
+++ Knockoutjs/Knockoutjs/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Knockoutjs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Knockoutjs.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Names names = new Names();
+            string firstname = names.FirstName;
+            string lastname = names.LastName;
+
+            return View();
+        }
+    }
+}
---
+++ Knockoutjs/Knockoutjs/Models/Names.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Knockoutjs.Models
+{
+    public class Names
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
